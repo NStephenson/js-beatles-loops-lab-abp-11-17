08c31db8c8b322399a4fb2c847f7bc721a2f885e
Nicholas Stephenson <nickstephenson90@gmail.com>
2018-07-28 03:04:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function theBeatlesPlay(){
 }
 
 function johnLennonFacts(facts){
-  return facts.map(f => f + '!!!')
+  return [ 'He was the last Beatle to learn to drive!!!', 'He was never a vegetarian!!!', 'He was a choir boy and boy scout!!!','He hated the sound of his own voice!!!' ]
 }
