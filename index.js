393e3d61d724367e596e34d90413455a76697e6f
Nicholas Stephenson <nickstephenson90@gmail.com>
2018-07-28 03:02:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function theBeatlesPlay(){
   return [ 'John Lennon plays Guitar', 'Paul McCartney plays Bass Guitar', 'George Harrison plays Lead Guitar', 'Ringo Starr plays Drums' ]
 }
+
+function johnLennonFacts(){
+  
+}
