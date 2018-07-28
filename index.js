026500236f570df10d45f2e8dd721341738378c2
Nicholas Stephenson <nickstephenson90@gmail.com>
2018-07-28 03:03:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function theBeatlesPlay(){
   return [ 'John Lennon plays Guitar', 'Paul McCartney plays Bass Guitar', 'George Harrison plays Lead Guitar', 'Ringo Starr plays Drums' ]
 }
 
-function johnLennonFacts(){
+function johnLennonFacts(facts){
   return [ 'He was the last Beatle to learn to drive!!!', 'He was never a vegetarian!!!', 'He was a choir boy and boy scout!!!','He hated the sound of his own voice!!!' ]
 }
